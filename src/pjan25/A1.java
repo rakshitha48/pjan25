b9bf19768347ad33922c48d6ad848ac92ec1686a
rakshitha48 <46997250+rakshitha48@users.noreply.github.com>
2019-01-25 16:09:19 +0530

    Initial commit

---
+++ src/pjan25/A1.java
@@ -0,0 +1,9 @@
+package pjan25;
+
+public class A1 {
+
+	public static void main(String[] args) {
+System.out.println("Hi");
+	}
+
+}
---
+++ src/pjan25/A2.java
@@ -0,0 +1,9 @@
+package pjan25;
+
+public class A2 {
+
+	public static void main(String[] args) {
+System.out.println("qsp");
+	}
+
+}
